6c14a858d8c51c70fcbbc9e7320ded3c067c9496
linearza <vanwijk.mc@gmail.com>
2023-05-11 09:23:22 +0200

    fix: fix reset script, remove misleading npm scripts

--- reset.js
+++ reset.js
@@ -6,7 +6,7 @@ console.info(
   `\x1b[94me2cct: Resetting your git state... \x1b[0m`
 );
 
-const result = spawnSync('sh', ['-c', 'git reset --hard && git clean -f']);
+const result = spawnSync('sh', ['-c', 'git reset HEAD~ && git reset HEAD~ && git reset --hard && git clean -f']);
 
 if (result.status !== 0) {
   console.error(`e2cct: Reset failed with code ${result.status}: ${result.stderr}`);
