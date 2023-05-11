f4975164eb49c12df3a6a847d87a5387623ae0aa
linearza <vanwijk.mc@gmail.com>
2023-05-11 17:08:58 +0200

    feat: adds new utils file

---
+++ shared/utils.js
@@ -0,0 +1,18 @@
+
+const { resolve } = require("path")
+const path = require("path");
+
+module.exports.getFilePaths = function(args) {
+  const filePath = args.file
+  const dirPath = path.dirname(filePath);
+  const extName = path.extname(filePath);
+  const baseName = path.basename(filePath, extName);
+
+  const absoluteCoffeePath = resolve(`${dirPath}/${baseName}.coffee`);
+  const absoluteJsFilePath = resolve(`${dirPath}/${baseName}.js`);
+
+  return {
+    absCsPath: absoluteCoffeePath,
+    absJsPath: absoluteJsFilePath
+  }
+}
