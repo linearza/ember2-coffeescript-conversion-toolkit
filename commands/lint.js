6ea7aea46826f367b26efad5909318426f275a15
linearza <vanwijk.mc@gmail.com>
2023-05-15 22:46:14 +0200

    chore: update linting to do triple pass

--- commands/lint.js
+++ commands/lint.js
@@ -10,22 +10,42 @@ module.exports = function (args) {
     if (!fs.existsSync(absJsPath)) {
       return console.error('File does not exist yet, did you mean to convert first? File: \n', absJsPath)
     }
-      const dependency = {
-        description: "Fixing eslint issues...",
+
+    const dependencies = [
+      {
+        description: "Fixing possible issues... Pass 1",
         name: "eslint",
         binary: "npx",
         command: `eslint`,
-        args: [`${absJsPath}`, "--fix"],
+        args: [`${absJsPath}`, "--fix",],
+      },
+      {
+        description: "Fixing possible issues... Pass 2",
+        name: "eslint",
+        binary: "npx",
+        command: `eslint`,
+        args: [`${absJsPath}`, "--fix",],
+      },
+      {
+        description: "Fixing possible issues... Pass 3",
+        name: "eslint",
+        binary: "npx",
+        command: `eslint`,
+        args: [`${absJsPath}`, "--fix",],
       }
-    
-      console.info(`\x1b[94m${dependency.name}: ${dependency.description} \x1b[0m`);
+    ]
+
+    for (const dependency of dependencies) {
+      console.info(
+        `\x1b[94m${dependency.name}: ${dependency.description} \x1b[0m`
+      );
     
       const result = spawnSync(
         dependency.binary,
         [dependency.command, ...dependency.args],
-        { stdio: ["inherit", "inherit", "pipe"] }
+        { stdio: ["inherit", "inherit", "pipe"], shell: true }
       );
-    
+      
       if (result.status === 2) {
         console.error(
           `Command failed: ${dependency.name}, command: ${
@@ -34,8 +54,14 @@ module.exports = function (args) {
         );
         console.error(result.output[2].toString());
         process.exitCode = 1;
+        break;
       }
-    
+    }
+
+    console.info(
+      `\x1b[94mLinting complete, remember to manually fix the outstanding ones! \x1b[0m`
+    );
+
   } catch(err) {
     console.error('lint:', err)
   }
